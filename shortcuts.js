d833cdd32f348ca8c767ce0b0549066dcce46178
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-02-15 15:08:23 -0800

    Implemented basic functionality to show command and accept input

---
+++ shortcuts.js
@@ -0,0 +1,30 @@
+
+var shortcuts = [
+
+  {
+    command: 'join lines',
+    correct: '⌘J',
+    keys: "meta j" 
+  },
+  {
+    command: 'cut line',
+    correct: '⌘X',
+    keys: 'meta x' 
+  },
+  {
+    command: 'goto line in current file',
+    correct: '^G',
+    keys: "ctrl g" 
+  },
+  {
+    command: 'soft undo',
+    correct: '⌘U',
+    keys: "meta u" 
+  },
+  {
+    command: 'use selection for find',
+    correct: '⌘E',
+    keys: 'meta e' 
+  }
+  
+];
