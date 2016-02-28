048c77b8dca86878d19dcece450347315f6a4dbb
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-02-27 18:55:17 -0800

    Added more sublime shortcuts

--- src/sublime.js
+++ src/sublime.js
@@ -2,19 +2,9 @@
 var sublimeShortcuts = [
 
   {
-    command: 'Goto line in current file',
-    correct: '^ + G',
-    keys: "ctrl g" 
-  },
-  {
-    command: 'Paste and indent correctly',
-    correct: '⌘ + ⇧ + V',
-    keys: 'meta shift v' 
-  },
-  {
-    command: 'Use selection for find',
-    correct: '⌘ + E',
-    keys: 'meta e' 
+    command: 'Repeat last keyboard shortcut command',
+    correct: '⌘ + Y',
+    keys: "meta y" 
   },
   {
     command: 'Block comment current selection',
@@ -22,79 +12,14 @@ var sublimeShortcuts = [
     keys: 'meta option /' 
   },
   {
-    command: 'Select contents of current parentheses',
-    correct: '⌃ + ⇧ + M',
-    keys: "ctrl shift m" 
-  },
-  {
-    command: 'Split view into two columns',
-    correct: '⌘ + ⌥ + 2',
-    keys: 'meta option 2' 
-  },
-  {
-    command: 'Duplicate line',
-    correct: '⌘ + ⇧ + D',
-    keys: 'meta shift d' 
-  },
-  {
-    command: 'Goto matching parentheses',
-    correct: '⌃ + M',
-    keys: 'ctrl m' 
-  },
-  {
-    command: 'Comment current line',
+    command: 'Comment current line(s)',
     correct: '⌘ + /',
     keys: 'meta /' 
   },
   {
-    command: 'Repeat last keyboard shortcut command',
-    correct: '⌘ + Y',
-    keys: "meta y" 
-  },
-  {
-    command: 'Join lines',
-    correct: '⌘ + J',
-    keys: "meta j" 
-  },
-  {
-    command: 'Select word',
-    correct: '⌘ + D',
-    keys: "meta d" 
-  },
-  {
-    command: 'Find next',
-    correct: '⌘ + G',
-    keys: "meta g" 
-  },
-  {
-    command: 'Find in files',
-    correct: '⌘ + ⇧ + F',
-    keys: "meta shift f" 
-  },
-  {
-    command: 'Cut line',
-    correct: '⌘ + X',
-    keys: 'meta x' 
-  },
-  {
-    command: 'Toggle fullscreen mode',
-    correct: '⌃ + ⌘ + F',
-    keys: 'ctrl meta f' 
-  },
-  {
-    command: 'Soft undo',
-    correct: '⌘ + U',
-    keys: "meta u" 
-  },
-  {
-    command: 'Indent current line(s)',
-    correct: '⌘ + ]',
-    keys: 'meta ]' 
-  },
-  {
-    command: 'Delete current line',
-    correct: '⌃ + ⇧ + K',
-    keys: "ctrl shift k" 
+    command: 'Duplicate line(s)',
+    correct: '⌘ + ⇧ + D',
+    keys: 'meta shift d' 
   },
   {
     command: 'Un-indent current line(s)',
@@ -102,9 +27,234 @@ var sublimeShortcuts = [
     keys: "meta [" 
   },
   {
+    command: 'Indent current line(s)',
+    correct: '⌘ + ]',
+    keys: 'meta ]' 
+  },
+  {
+    command: 'Insert line before',
+    correct: '⌘ + ⇧ + ↵',
+    keys: 'meta shift enter'  
+  },
+  {
+    command: 'Select contents of current parentheses',
+    correct: '⌃ + ⇧ + M',
+    keys: "ctrl shift m" 
+  },
+  {
+    command: 'Goto matching parentheses',
+    correct: '⌃ + M',
+    keys: 'ctrl m' 
+  },
+  {
+    command: 'Select word',
+    correct: '⌘ + D',
+    keys: "meta d" 
+  },
+  {
+    command: 'Select line',
+    correct: '⌘ + L',
+    keys: "meta l" 
+  },
+  {
+    command: 'Insert line after',
+    correct: '⌘ + ↵',
+    keys: 'meta enter'  
+  },
+  {
+    command: 'Cut line',
+    correct: '⌘ + X',
+    keys: 'meta x' 
+  },
+  {
+    command: 'Paste from history',
+    correct: '⌘ + ⌥ + V',
+    keys: 'meta option v' 
+  },
+  {
+    command: 'Paste and indent correctly',
+    correct: '⌘ + ⇧ + V',
+    keys: 'meta shift v' 
+  },
+  {
+    command: 'Soft undo',
+    correct: '⌘ + U',
+    keys: "meta u" 
+  },
+  {
+    command: 'Use selection for replace',
+    correct: '⌘ + E',
+    keys: "meta e" 
+  },
+  {
+    command: 'Use selection for find',
+    correct: '⌘ + E',
+    keys: 'meta e' 
+  },
+  {
+    command: 'Select current indentation',
+    correct: '⌘ + ⇧ + J',
+    keys: 'meta shift j' 
+  },
+  {
+    command: 'Select between brackets',
+    correct: '⌃ + ⇧ + M',
+    keys: "ctrl shift m" 
+  },
+  {
+    command: 'Goto line in current file',
+    correct: '^ + G',
+    keys: "ctrl g" 
+  },
+  {
     command: 'Goto symbol',
     correct: '⌘ + R',
     keys: 'meta r' 
+  },
+  {
+    command: 'Quick open files by name',
+    correct: '⌘ + P',
+    keys: 'meta p' 
+  },
+  {
+    command: 'Quick find',
+    correct: '⌘ + ⌥ + G',
+    keys: 'meta option g' 
+  },
+  {
+    command: 'Replace next',
+    correct: '⌘ + ⌥ + E',
+    keys: 'meta option e' 
+  },
+  {
+    command: 'Find previous',
+    correct: '⌘ + ⇧ + G',
+    keys: 'meta shift g' 
+  },
+  {
+    command: 'Find next',
+    correct: '⌘ + G',
+    keys: 'meta g' 
+  },
+  {
+    command: 'Find in files',
+    correct: '⌘ + ⇧ + F',
+    keys: "meta shift f" 
+  },
+  {
+    command: 'Find',
+    correct: '⌘ + F',
+    keys: "meta f" 
+  },
+  {
+    command: 'Toggle full screen',
+    correct: '⌘ + ^ + F',
+    keys: "meta ctrl f" 
+  },
+  {
+    command: 'Toggle console',
+    correct: '^ + `',
+    keys: "ctrl `" 
+  },
+  {
+    command: 'Save all',
+    correct: '⌘ + ⌥ + S',
+    keys: 'meta option s' 
+  },
+  {
+    command: 'Save as',
+    correct: '⌘ + ⇧ + S',
+    keys: 'meta shift s' 
+  },
+  {
+    command: 'Zoom out',
+    correct: '⌘ + -',
+    keys: "meta -" 
+  },
+  {
+    command: 'User settings',
+    correct: '⌘ + ,',
+    keys: "meta ," 
+  },
+  {
+    command: 'Zoom in',
+    correct: '⌘ + =',
+    keys: "meta =" 
+  },
+  {
+    command: 'Open file',
+    correct: '⌘ + O',
+    keys: "meta o" 
+  },
+  {
+    command: 'Command prompt',
+    correct: '⌘ + ⇧ + P',
+    keys: 'meta shift p' 
+  },
+  {
+    command: 'Hide all other windows',
+    correct: '⌘ + ⌥ + H',
+    keys: 'meta option h' 
+  },
+  {
+    command: 'Hide sublime text',
+    correct: '⌘ + H',
+    keys: 'meta h' 
+  },
+  {
+    command: 'Join lines',
+    correct: '⌘ + J',
+    keys: "meta j" 
+  },
+  {
+    command: 'Delete current line(s)',
+    correct: '⌃ + ⇧ + K',
+    keys: "ctrl shift k" 
+  },
+  {
+    command: 'Split view into two columns',
+    correct: '⌘ + ⌥ + 2',
+    keys: 'meta option 2' 
+  },
+  {
+    command: 'Split view into three columns',
+    correct: '⌘ + ⌥ + 3',
+    keys: 'meta option 3' 
+  },
+  {
+    command: 'Split view into four columns',
+    correct: '⌘ + ⌥ + 4',
+    keys: 'meta option 4' 
+  },
+  {
+    command: 'Split view into grid of four',
+    correct: '⌘ + ⌥ + 5',
+    keys: 'meta option 5' 
+  },
+  {
+    command: 'Revert view to single column',
+    correct: '⌘ + ⌥ + 1',
+    keys: 'meta option 1' 
+  },
+  {
+    command: 'Jump to column or group 1',
+    correct: '^ + 1',
+    keys: "ctrl 1" 
+  },
+  {
+    command: 'Jump to column or group 2',
+    correct: '^ + 2',
+    keys: "ctrl 2" 
+  },
+  {
+    command: 'Move file to column or group 3',
+    correct: '^ + ⇧ + 3',
+    keys: "ctrl shift 3" 
+  },  
+  {
+    command: 'Move file to column or group 4',
+    correct: '^ + ⇧ + 4',
+    keys: "ctrl shift 4" 
   }
   
 ];
