ac3acda91f365b166637694b051d75990a9428fd
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-02-28 22:25:23 -0800

    Added atom shortcuts

--- src/atom.js
+++ src/atom.js
@@ -1,55 +1,251 @@
 
-var atomShortcuts = [
+var atomShortcuts = {
+  set1: [
+    {
+      command: 'Join lines',
+      correct: '⌘ + J',
+      keys: 'meta j' 
+    },
+    {
+      command: 'Fold code',
+      correct: '⌘ + ⌥ + [',
+      keys: 'meta option [' 
+    },
+    {
+      command: 'Unfold code',
+      correct: '⌘ + ⌥ + ]',
+      keys: 'meta option ]' 
+    },
+    {
+      command: 'Unfold all code',
+      correct: '⌘ + ⌥ + }',
+      keys: 'meta option }' 
+    },
+    {
+      command: 'Move to beginning of word',
+      correct: '⌥ + B',
+      keys: 'option b' 
+    },
+    {
+      command: 'Move to end of word',
+      correct: '⌥ + F',
+      keys: 'option f' 
+    },
+    {
+      command: 'Move to beginning of line',
+      correct: '⌃ + A',
+      keys: 'ctrl a' 
+    },
+    {
+      command: 'Move to end of line',
+      correct: '⌃ + E',
+      keys: 'ctrl e' 
+    },
+    {
+      command: 'Move to first character of line',
+      correct: '⌘ + LEFT',
+      keys: 'meta left'  
+    },
+    {
+      command: 'Move to last character of line',
+      correct: '⌘ + RIGHT',
+      keys: 'meta right'  
+    },
+    {
+      command: 'Goto matching bracket',
+      correct: '⌃ + M',
+      keys: 'ctrl m' 
+    },
+    {
+      command: 'Goto line',
+      correct: '^ + G',
+      keys: 'ctrl g' 
+    }
+  ],
 
-  {
-    command: 'Goto line',
-    correct: '^ + G',
-    keys: "ctrl g" 
-  },
-  {
-    command: 'Goto matching bracket',
-    correct: '^ + M',
-    keys: "ctrl m" 
-  },
-  {
-    command: 'Delete line',
-    correct: '^ + ⇧ + K',
-    keys: "ctrl shift k" 
-  },
-  {
-    command: 'Cut to end of line',
-    correct: '^ + K',
-    keys: "ctrl k" 
-  },
-  {
-    command: 'Move to beginning of word',
-    correct: '⌥ + B',
-    keys: "option b" 
-  },
-  {
-    command: 'Duplicate line',
-    correct: '⌘ + ⇧ + D',
-    keys: 'meta shift d' 
-  },
-  {
-    command: 'Delete to end of word',
-    correct: '⌥ + D',
-    keys: 'option d' 
-  },
-  {
-    command: 'Join lines',
-    correct: '⌘ + J',
-    keys: "meta j" 
-  },
-  {
-    command: 'Move to end of word',
-    correct: '⌥ + F',
-    keys: "option f" 
-  },
-  {
-    command: 'Select left character',
-    correct: '^ + ⇧ + B',
-    keys: 'ctrl shift b' 
-  }
-  
-];
+  set2: [
+    {
+      command: 'Select left character',
+      correct: '⇧ + ^ + B',
+      keys: 'shift ctrl b' 
+    },
+    {
+      command: 'Select right character',
+      correct: '⇧ + ^ + F',
+      keys: 'shift ctrl f' 
+    },
+    {
+      command: 'Select to beginning of word',
+      correct: '⇧ + ⌥ + B',
+      keys: 'shift option b' 
+    },
+    {
+      command: 'Select to end of word',
+      correct: '⇧ + ⌥ + F',
+      keys: 'shift option f' 
+    },
+    {
+      command: 'Select entire word',
+      correct: '⇧ + ⌃ + W',
+      keys: 'shift ctrl w' 
+    },
+    {
+      command: 'Select next',
+      correct: '⌘ + D',
+      keys: 'meta d' 
+    },
+    {
+      command: 'Select line`',
+      correct: '⌘ + L',
+      keys: 'meta l' 
+    },
+    {
+      command: 'Cut to end of line',
+      correct: '⌃ + K',
+      keys: 'ctrl k' 
+    },
+    {
+      command: 'Delete to end of word',
+      correct: '⌥ + D',
+      keys: 'option d' 
+    },
+    {
+      command: 'Delete line',
+      correct: '⌃ + ⇧ + K',
+      keys: 'ctrl shift k' 
+    },
+    {
+      command: 'Outdent selected text',
+      correct: '⌘ + [',
+      keys: 'meta [' 
+    },
+    {
+      command: 'Indent selected text',
+      correct: '⌘ + ]',
+      keys: 'meta ]' 
+    }
+  ],
+
+  set3: [
+    {
+      command: 'Duplicate line',
+      correct: '⌘ + ⇧ + D',
+      keys: 'meta shift d' 
+    },
+    {
+      command: 'Find string in current file',
+      correct: '⌘ + F',
+      keys: 'meta f' 
+    },
+    {
+      command: 'Find modified, untracked (git) file',
+      correct: '⌘ + ⇧ + B',
+      keys: 'meta shift b' 
+    },
+    {
+      command: 'Find string in all project files',
+      correct: '⌘ + ⇧ + F',
+      keys: 'meta shift f' 
+    },
+    {
+      command: 'Find next matching string',
+      correct: '⌘ + G',
+      keys: 'meta g' 
+    },
+    {
+      command: 'Find previous matching string',
+      correct: '⌘ + ⇧ + G',
+      keys: 'meta shift g' 
+    },
+    {
+      command: 'Select next matching string',
+      correct: '⌘ + D',
+      keys: 'meta d' 
+    },
+    {
+      command: 'Select all matching strings',
+      correct: '⌘ + ⌃ + G',
+      keys: 'meta ctrl g' 
+    },
+    {
+      command: 'Replace string in current file',
+      correct: '⌘ + ⌥ + F',
+      keys: 'meta option f' 
+    },
+    {
+      command: 'Search for symbol in file',
+      correct: '⌘ + R',
+      keys: 'meta r' 
+    },
+    {
+      command: 'Search for symbol across project',
+      correct: '⌘ + ⇧ + R',
+      keys: 'meta shift r' 
+    },
+    {
+      command: 'Find open file',
+      correct: '⌘ + B',
+      keys: 'meta b' 
+    }
+  ],
+
+
+  set4: [
+    {
+      command: 'Open command palette',
+      correct: '⌘ + ⇧ + P',
+      keys: 'meta shift p' 
+    },
+    {
+      command: 'Open configuration editor',
+      correct: '⌘ + ,',
+      keys: 'meta ,' 
+    },
+    {
+      command: 'Toggle full screen',
+      correct: '⌘ + ^ + F',
+      keys: 'meta ctrl f' 
+    },
+    {
+      command: 'Toggle line comments',
+      correct: '⌘ + /',
+      keys: 'meta /' 
+    },
+    {
+      command: 'Open tree view',
+      correct: '⌃ + O',
+      keys: 'ctrl o' 
+    },
+    {
+      command: 'Hide other applications',
+      correct: '⌘ + ⌥ + H',
+      keys: 'meta option h' 
+    },
+    {
+      command: 'Hide atom',
+      correct: '⌘ + H',
+      keys: 'meta h' 
+    },
+    {
+      command: 'Add a file',
+      correct: 'A',
+      keys: 'a' 
+    },
+    {
+      command: 'Move a file',
+      correct: 'M',
+      keys: 'm' 
+    },
+    {
+      command: 'Decrease font size',
+      correct: '⌘ + -',
+      keys: 'meta -' 
+    },
+    {
+      command: 'Increase font size',
+      correct: '⌘ + =',
+      keys: 'meta =' 
+    }
+  ]
+
+};
